04cecf7bdac4b497a851d6fcbcfdeff41c9a4a1f
babakfp <babak.bxf@gmail.com>
2024-10-26 09:28:04 +0330

    Added import formatter

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,7 @@
-/** @type {import("prettier").Config} */
+/** @type {import("prettier").Config & import("@ianvs/prettier-plugin-sort-imports").PluginConfig} */
 export default {
     semi: false,
     tabWidth: 4,
     experimentalTernaries: true,
+    plugins: ["@ianvs/prettier-plugin-sort-imports"],
 }
