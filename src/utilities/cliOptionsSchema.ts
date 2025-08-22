3997169b52a2d567188b7cef65ee3caeee31ff2b
babakfp <babak.bxf@gmail.com>
2025-08-23 00:31:29 +0330

    chore: better validation

--- src/utilities/cliOptionsSchema.ts
+++ src/utilities/cliOptionsSchema.ts
@@ -1,8 +1,8 @@
 import * as v from "valibot"
 
 export const cliOptionsSchema = v.object({
-    url: v.optional(v.pipe(v.string(), v.minLength(1))),
-    email: v.optional(v.pipe(v.string(), v.minLength(1))),
+    url: v.optional(v.pipe(v.string(), v.url())),
+    email: v.optional(v.pipe(v.string(), v.email())),
     password: v.optional(v.pipe(v.string(), v.minLength(1))),
     output: v.pipe(v.string(), v.minLength(1)),
     env: v.optional(v.pipe(v.string(), v.minLength(1))),
